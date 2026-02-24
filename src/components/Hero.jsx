298da8480370ef01b4e3d9ef7edfa49df0ebc0dd
AminaSalik <aminasalik012@gmail.com>
2026-02-23 18:56:49 -0800

    ProtectedRoute

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ export default function Hero() {
      <ProblemSolut/>
       <Benefits_ />
       <Services />
-      <Booking />
+      {/* <Booking /> */}
       <Footer />
       <ScrollToTopBtn />
     </>
